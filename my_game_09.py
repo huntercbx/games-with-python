1aa64bf0347c52f3a2602322ecb6023eb1e8f4cb
Alexander Alexeyev <huntercbx@gmail.com>
2017-03-29 07:31:51 +0300

    Shooting

---
+++ my_game_09.py
@@ -0,0 +1,147 @@
+import pygame
+import os
+
+# ширина и высота игрового экрана
+WIDTH = 640
+HEIGHT = 480
+# частота кадров
+FPS = 60
+
+# путь к изображениям
+game_folder = os.path.dirname(__file__)
+img_folder = os.path.join(game_folder, "images");
+
+# класс для корабля игрока
+class PlayerShip(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join(img_folder, "player_ship.png")).convert()
+        self.image.set_colorkey((0, 0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.x = 10
+        self.rect.centery = HEIGHT / 2
+        self.last_shot = pygame.time.get_ticks()
+
+    def shoot(self):
+        now = pygame.time.get_ticks()
+        if now - self.last_shot > 250:
+            self.last_shot = now
+            laser = Laser(self.rect.right, self.rect.centery)
+            lasers.add(laser)
+            sprites.add(laser)
+
+    def update(self):
+        # обработка нажатия клавиш
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_UP]:
+            self.rect.y -= 8
+        if keystate[pygame.K_DOWN]:
+            self.rect.y += 8
+        if keystate[pygame.K_LEFT]:
+            self.rect.x -= 8
+        if keystate[pygame.K_RIGHT]:
+            self.rect.x += 8
+        if keystate[pygame.K_SPACE]:
+            self.shoot()
+
+        # проверка выхода за границы экрана
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.top < 0:
+            self.rect.top = 0
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join(img_folder, "meteor.png")).convert()
+        self.image.set_colorkey((0, 0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.left = x
+        self.rect.top = y
+
+    def update(self):
+        self.rect.left -= 3
+        
+        # удалить метеор после выхода за границы экрана
+        if self.rect.right < -10:
+            self.kill()
+
+# класс для выстрела
+class Laser(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join(img_folder, "laser.png")).convert()
+        self.image.set_colorkey((0, 0, 0))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.centery = y
+        self.speedx = 10
+        self.speedy = 0
+
+    def update(self):
+        self.rect.left += self.speedx
+        self.rect.top += self.speedy
+        
+        # удалить выстрел после выхода за границы экрана
+        if self.rect.left > WIDTH + 10:
+            self.kill()
+
+# инициализация библиотеки pygame
+pygame.init()
+
+# создание объекта для отслеживания времени
+clock = pygame.time.Clock()
+
+# создание игрового экрана
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# смена залоголовка окна
+pygame.display.set_caption("My Game")
+
+# все спрайты будут храниться здесь
+sprites = pygame.sprite.Group()
+meteors = pygame.sprite.Group()
+lasers = pygame.sprite.Group()
+player = PlayerShip()
+sprites.add(player)
+
+meteor = Meteor(WIDTH - 50, 40)
+sprites.add(meteor)
+meteors.add(meteor)
+meteor = Meteor(WIDTH - 100, 200)
+sprites.add(meteor)
+meteors.add(meteor)
+
+# цикл событий
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # изменение движения
+    sprites.update()
+
+    # обнаружение столкновений
+    hits = pygame.sprite.groupcollide(lasers, meteors, True, True)
+    
+    hits = pygame.sprite.spritecollide(player, meteors, False)
+    if hits:
+        running = False
+
+    # очистка фона и рисование спрайтов
+    screen.fill((0, 0, 80))
+    sprites.draw(screen)
+
+    # переключение буферов
+    pygame.display.flip()
+
+    # задает частоту запуска цикла
+    clock.tick(FPS)
+
+# завершение работы библиотеки pygame
+pygame.quit()
